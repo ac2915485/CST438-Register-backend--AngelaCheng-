776ba66729d0f716f19b0ccbf5e7f6cf65af3f8e
AngelaCheng <acheng@csumb.edu>
2023-07-18 22:11:03 -0700

    Part 2 - Develop and test Asynchronous Message communication

--- cst438_register/src/main/java/com/cst438/controller/CourseController.java
+++ cst438_register/src/main/java/com/cst438/controller/CourseController.java
@@ -1,15 +1,25 @@
 package com.cst438.controller;
 
+import java.sql.Date;
+
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
+import com.cst438.domain.Course;
 import com.cst438.domain.CourseDTOG;
 import com.cst438.domain.Enrollment;
 import com.cst438.domain.EnrollmentRepository;
+import com.cst438.domain.Student;
 
 @RestController
 public class CourseController {
